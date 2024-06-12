e6920c31a9c0fa04ab1c6bfa9c9975fb7f7c4817
Onrirr <ulughann@gmail.com>
2024-06-12 12:24:21 +0300

    a

---
+++ README.md
@@ -0,0 +1,26 @@
+<h1 align="center">Wails Astro Template</h1>
+
+## Introductions
+
+This project is a [Wails](https://github.com/wailsapp/wails) template based on the [Astro](https://astro.build/).
+
+![wails-astro](https://res.cloudinary.com/dqkglfgx4/image/upload/v1674279127/wails_astro.png)
+
+## Getting Started
+
+**New Project**:
+
+```
+wails init -n <Your Appname> -t https://github.com/hnggngn/wails-astro -g -ide vscode
+```
+
+flag description:
+
+-   n - The name of the application to be created
+-   t - Template name, supports built-in template names and third-party templates in the form of hyperlinks
+-   g - Initialise git repository
+-   ide Generate IDE project files
+
+## License
+
+[License MIT](./LICENSE)
---
+++ main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"embed"
+
+	"github.com/wailsapp/wails/v2"
+	"github.com/wailsapp/wails/v2/pkg/options"
+	"github.com/wailsapp/wails/v2/pkg/options/assetserver"
+	"github.com/wailsapp/wails/v2/pkg/options/windows"
+)
+
+//go:embed all:frontend/dist
+var assets embed.FS
+
+func main() {
+	// Create an instance of the app structure
+	app := NewApp()
+
+	// Create application with options
+	err := wails.Run(&options.App{
+		Title:  "noter",
+		Width:  900,
+		Height: 700,
+		AssetServer: &assetserver.Options{
+			Assets: assets,
+		},
+		OnStartup: app.startup,
+		Bind: []interface{}{
+			app,
+		},
+		Windows: &windows.Options{
+			WindowIsTranslucent: true,
+		},
+	})
+
+	if err != nil {
+		println("Error:", err.Error())
+	}
+}
