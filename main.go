0f690ebd40f1f3d5c69d0e5d6a21c15d1e544348
Onrirr <ulughann@gmail.com>
2024-06-19 20:57:38 +0300

    ...

--- main.go
+++ main.go
@@ -6,7 +6,6 @@ import (
 	"github.com/wailsapp/wails/v2"
 	"github.com/wailsapp/wails/v2/pkg/options"
 	"github.com/wailsapp/wails/v2/pkg/options/assetserver"
-	"github.com/wailsapp/wails/v2/pkg/options/windows"
 )
 
 //go:embed all:frontend/dist
@@ -27,9 +26,6 @@ func main() {
 		OnStartup: app.startup,
 		Bind: []interface{}{
 			app,
-		},
-		Windows: &windows.Options{
-			WindowIsTranslucent: true,
 		},
 	})
 
